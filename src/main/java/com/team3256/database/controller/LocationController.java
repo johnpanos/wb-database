d2cfa3c469bbc45759a387494276bc113ef3bd8d
John Panos <johnnypanos@icloud.com>
2018-10-09 08:50:34 -0700

    Added error handling for location

--- src/main/java/com/team3256/database/controller/LocationController.java
+++ src/main/java/com/team3256/database/controller/LocationController.java
@@ -1,5 +1,6 @@
 package com.team3256.database.controller;
 
+import com.team3256.database.error.DatabaseNotFoundException;
 import com.team3256.database.model.inventory.Location;
 import com.team3256.database.model.inventory.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,39 +24,37 @@ public class LocationController {
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public Location getLocation(@PathVariable("id") Long id) {
         Optional<Location> locationOptional = locationRepository.findById(id);
-        if (locationOptional.isPresent()) {
-            return locationOptional.get();
-        } else {
-            // TODO: Implement error
-            return null;
+
+        if (!locationOptional.isPresent()) {
+            throw new DatabaseNotFoundException("no location with id - " + id);
         }
+
+        return locationOptional.get();
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
     public Location updateLocation(@RequestBody Location location) {
         Optional<Location> locationOptional = locationRepository.findById(location.getId());
 
-        if (locationOptional.isPresent()) {
-            Location dbLocation = locationOptional.get();
-            dbLocation.setName(location.getName());
-            locationRepository.save(dbLocation);
-            return dbLocation;
-        } else {
-            // TODO: Implement correct error
-            return null;
+        if (!locationOptional.isPresent()) {
+            throw new DatabaseNotFoundException("no location found with id - " + location.getId());
         }
+
+        Location dbLocation = locationOptional.get();
+        dbLocation.setName(location.getName());
+        locationRepository.save(dbLocation);
+        return dbLocation;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
     public Long deleteLocation(@PathVariable("id") Long id) {
         Optional<Location> location = locationRepository.findById(id);
 
-        if (location.isPresent()) {
-            locationRepository.delete(location.get());
-            return location.get().getId();
-        } else {
-            // TODO: Implement response
-            return (long) -1;
+        if (!location.isPresent()) {
+            throw new DatabaseNotFoundException("no location found with id - " + id);
         }
+        
+        locationRepository.delete(location.get());
+        return location.get().getId();
     }
 }
